2907a104adfafb4307dcf6edec5818b3e249f5c9
Dexter Industries <dxtrindustries@gmail.com>
2016-10-25 21:53:36 +0400

    Update camera-vision-logo.py

--- camera-vision-logo.py
+++ camera-vision-logo.py
@@ -1,5 +1,6 @@
 """
-Google Vision API Tutorial with a Raspberry Pi and Raspberry Pi Camera.  See more about it here:  https://www.dexterindustries.com/howto/use-google-cloud-vision-on-the-raspberry-pi/
+Google Vision API Tutorial with a Raspberry Pi and Raspberry Pi Camera.  
+See more about it here:  https://www.dexterindustries.com/howto/use-google-cloud-vision-on-the-raspberry-pi/
 
 Use Google Cloud Vision on the Raspberry Pi to take a picture with the Raspberry Pi Camera and classify it with the Google Cloud Vision API.   First, we'll walk you through setting up the Google Cloud Platform.  Next, we will use the Raspberry Pi Camera to take a picture of an object, and then use the Raspberry Pi to upload the picture taken to Google Cloud.  We can analyze the picture and return labels (what's going on in the picture), logos (company logos that are in the picture) and faces.
 
